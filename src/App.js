4476d6fd64c9fa136977824bb5c350513c079bc4
Benjamin Koehler <bkoehler751@gmail.com>
2021-09-09 20:27:48 -0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import NasaPhoto from './components/NasaPhoto';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   
+   <div className="app">
+     <Route component={Home} path="/" exact />
+     <Route component={NasaPhoto} path="/nasaphoto" />
+   </div>
+   </BrowserRouter>
   );
 }
 
